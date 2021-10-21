0ca31f9360971568733834c05948b90a07612094
MariaKobryn <kobinmary2508@gmail.com>
2021-10-22 00:40:57 +0300

    first master commit

---
+++ Lab3.3/Lab3.3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x; // вхідний аргумент
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x < -8 - R)
+		y = R;
+	else
+		if (-8 - R <= x && x <= -8 + R)
+			y = R - sqrt((R * R) - ((x + 8) * (x + 8)));
+		else
+			if (-8 + R < x && x <= -4)
+				y = R;
+			else
+				if (-4 < x && x <= 2)
+					y = (2 * R - x - 4 - R * x) / 6;
+				else
+					y = -3 + x;
+
+	cout << endl;
+	cout << "y = " << y << endl;
+
+	cin.get();
+	return 0;
+}
